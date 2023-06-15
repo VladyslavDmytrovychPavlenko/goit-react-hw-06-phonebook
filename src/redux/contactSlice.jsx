647c13c153df9ae34ca9e6bdf9c056228375dda3
VladyslavDmytrovychPavlenko <onlyforonlineworkvlad@gmail.com>
2023-06-15 20:00:16 +0300

    all

--- src/redux/contactSlice.jsx
+++ src/redux/contactSlice.jsx
@@ -1,33 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from '@reduxjs/toolkit';
 
-const contactSlice = createSlice({
+export const contactSlice = createSlice({
   name: 'contacts',
-  initialState: {
-    contacts: [
-      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  },
+
+  initialState: [
+    { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+    { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+    { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+    { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+  ],
+  filter: '',
+
   reducers: {
     addContact: (state, action) => {
-      const { name, number } = action.payload;
-      const newContact = { id: nanoid(), name, number };
-      state.contacts.push(newContact);
+      const contact = {
+        id: nanoid(),
+        name: action.payload.name,
+        number: action.payload.number,
+      };
+      state.push(contact);
     },
+
     deleteContact: (state, action) => {
-      const contactId = action.payload.id;
-      state.contacts = state.contacts.filter(contact => contact.id !== contactId);
-    },
-    setContactFilter: (state, action) => {
-      state.filter = action.payload;
+      return state.filter(contact => contact.id !== action.payload.id);
     },
   },
 });
 
-export const { addContact, deleteContact, setContactFilter } = contactSlice.actions;
+export const { addContact, deleteContact } = contactSlice.actions;
 
 export default contactSlice.reducer;
